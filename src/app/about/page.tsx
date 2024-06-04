e144f21a0b272b52fe090e63d7f60885c637252c
kotik322322 <kotan23111995@gmail.com>
2024-06-04 18:43:02 +0300

    fixed style font text AboutPage

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -15,11 +15,11 @@ const AboutPage = () => {
       <ParticlesComponent config={darkConfig} />
       <Logo textColor="#ffffff" />
 
-      <div className="w-full flex flex-col justify-center items-center gap-y-4">
+      <div className="w-full flex flex-col md:flex-row justify-center items-center  lg:gap-y-4 md:gap-x-10">
 
         {/* Profile Image */}
-        <div className="w-full relative rounded-2xl border-2 border-solid border-dark bg-light p-4 lg:p-8 ">
-          <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
+        <div className="w-full relative rounded-2xl border-2 border-solid border-dark bg-light p-4  my-10 lg:my-4 md:w-1/2 lg:w-1/3 ">
+          <div className="absolute top-0 -right-3 -z-10  w-[102%] h-[103%] lg:h-[102%] rounded-[2rem] bg-dark" />
           <Image
             src={profilePic}
             alt="Kostiantyn Marfin Profile Image"
@@ -29,12 +29,12 @@ const AboutPage = () => {
         {/* Profile Image */}
 
         {/* Biography */}
-        <div className="w-full flex flex-col items-start justify-start gap-y-4 text-light">
-          <h2 className="text-lg font-bold uppercase">
+        <div className="w-full flex flex-col items-start justify-start gap-y-4 text-light md:w-1/2 lg:w-2/3 text-sm lg:text-base">
+          <h2 className="text-lg lg:text-xl font-bold uppercase">
             Biography
           </h2>
 
-          <p className="font-medium text-sm">
+          <p className="font-medium">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, neque tenetur. Ea
             dolorem minus quam, exercitationem eos, magni quidem, inventore praesentium cupiditate
             ad perferendis. Voluptatibus, ullam enim quia dolorem incidunt impedit libero dolorum?
@@ -42,7 +42,7 @@ const AboutPage = () => {
             aliquam cumque ea maxime accusamus nesciunt!
           </p>
 
-          <p className=" font-medium text-sm">
+          <p className=" font-medium">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, neque tenetur. Ea
             dolorem minus quam, exercitationem eos, magni quidem, inventore praesentium cupiditate
             ad perferendis. Voluptatibus, ullam enim quia dolorem incidunt impedit libero dolorum?
@@ -50,7 +50,7 @@ const AboutPage = () => {
             aliquam cumque ea maxime accusamus nesciunt!
           </p>
 
-          <p className="font-medium text-sm">
+          <p className="font-medium">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, neque tenetur. Ea
             dolorem minus quam, exercitationem eos, magni quidem, inventore praesentium cupiditate
             ad perferendis. Voluptatibus, ullam enim quia dolorem incidunt impedit libero dolorum?
