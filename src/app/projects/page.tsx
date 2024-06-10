04ebece25d14a13d19eb84f2b4991b2553a61da0
kotik322322 <kotan23111995@gmail.com>
2024-06-11 00:33:56 +0300

    фввув вуыкшзешщт ещ ауфгегкув зкщоусе фтв зщкеащдшщ зкщоусе

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,6 +5,7 @@ import darkConfig from '../data/darkConfig.json';
 import Logo from '../components/Logo';
 import FeaturedProject from '../components/FeaturedProject';
 import project1 from '../../../public/website1.jpg';
+import project2 from '../../../public/website2.jpg';
 import Project from '../components/Project';
 
 const ProjectsPage = () => {
@@ -18,9 +19,9 @@ const ProjectsPage = () => {
       <div className="grid grid-cols-12 gap-12 gap-x-0 lg:gap-x-16 mt-10">
         <div className="col-span-12">
           <FeaturedProject
-            title="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum."
-            summary="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum."
-            link={'/link'}
+            title="Nike"
+            summary="For my portfolio, I developed a mock Nike website using Next.js, Redux Toolkit, MongoDB, TailwindCSS, Framer Motion, and TypeScript. This project showcases my ability to create responsive, dynamic web applications with a sleek design and smooth animations. It demonstrates my skills in full-stack development, state management, and modern frontend technologies."
+            link={'https://github.com/kotik322322/next-nike-v2'}
             type={'Featured Project'}
             img={project1}
             github={'https://github.com/kotik322322/next-nike-v2'}
@@ -28,12 +29,12 @@ const ProjectsPage = () => {
         </div>
         <div className="col-span-12 lg:col-span-6">
           <Project
-            title="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum."
-            summary="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum."
-            link={'/link'}
-            type={'Website'}
-            img={project1}
-            github={'https://github.com/kotik322322/next-nike-v2'}
+            title="Portfolio Website"
+            summary="This is my portfolio website project. It showcases my skills and expertise in web development using Next.js, TailwindCSS, Framer Motion, and TypeScript. The site is designed to provide a dynamic and interactive user experience while highlighting my professional capabilities."
+            link={'https://github.com/kotik322322/kostiantyndev'}
+            type={'Project'}
+            img={project2}
+            github={'https://github.com/kotik322322/kostiantyndev'}
           />
         </div>
       </div>
